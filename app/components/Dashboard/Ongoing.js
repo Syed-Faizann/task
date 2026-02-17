cc7fbe0632fd761425dfcbae5dd90ade2272438d
Syed-Faizann <syedfaizanali2122@gmail.com>
2026-02-17 17:36:23 +0500

    Ongoing

--- app/components/Dashboard/Ongoing.js
+++ app/components/Dashboard/Ongoing.js
@@ -7,8 +7,8 @@ const Ongoing = () => {
   return (
     <div className="w-full   ">
       <div className="bg-[#F0F0F3] rounded-xl shadow-md flex flex-col md:flex-row justify-between mb-6 overflow-hidden hover:shadow-lg transition-shadow duration-300">
-        <div className="p-4 md:p-6 ">
-          <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-3 sm:gap-4 flex-1">
+        <div className="p-4 md:p-6 flex-1">
+          <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-3 sm:gap-4">
             <div className="flex flex-col w-full sm:w-auto">
               <h2 className="text-base items-center md:text-lg flex xs:flex-row xs:items-center gap-1 sm:gap-2 text-[#282828]">
                 <span className="text-lg sm:text-xl md:text-2xl font-bold whitespace-nowrap">
@@ -19,9 +19,10 @@ const Ongoing = () => {
                 </span>
               </h2>
             </div>
+            <div className="hidden sm:block flex-1"></div>
 
             <div className="flex items-center">
-              <div className=" text-[#282828] whitespace-nowrap">
+              <div className="text-[#282828] whitespace-nowrap">
                 <span className="text-sm sm:text-base md:text-xl text-[#4C4D4E] xs:ml-0 sm:ml-0">
                   Second timer
                 </span>
@@ -29,7 +30,9 @@ const Ongoing = () => {
             </div>
           </div>
 
-          <p className="text-[#222222] pt-4 md:text-base text-[13px]">Add, remove questions</p>
+          <p className="text-[#222222] pt-4 md:text-base text-[13px]">
+            Add, remove questions
+          </p>
         </div>
 
         <div className="relative w-full md:w-[233px] h-[140px] sm:h-[160px] md:min-h-[160px]">
